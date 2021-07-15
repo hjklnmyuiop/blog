55c1b9e4f7c93d2320cb540c6c1ea5a81e06449d
dev_guo <Dev_guo@patazon.net>
2021-07-15 10:15:35 +0800

    调整配置

--- src/main/java/com/blog/config/ElasticSearchConfig.java
+++ src/main/java/com/blog/config/ElasticSearchConfig.java
@@ -3,16 +3,19 @@ package com.blog.config;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticSearchConfig {
+    @Value("${elasticsearh.host}")
+    public String esHost;
     @Bean
     public RestHighLevelClient restHighLevelClient(){
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
-                    new HttpHost("47.107.65.114", 9200, "http")
+                    new HttpHost(esHost, 9200, "http")
                 )
         );
         return  client;
