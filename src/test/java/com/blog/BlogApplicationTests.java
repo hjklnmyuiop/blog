9b649c9b30564aec7d3cc0d0660aa622a1ad521d
dev_guo <Dev_guo@patazon.net>
2021-07-15 09:52:07 +0800

    忽略文件

--- src/test/java/com/blog/BlogApplicationTests.java
+++ src/test/java/com/blog/BlogApplicationTests.java
@@ -44,109 +44,107 @@ class BlogApplicationTests {
     private BlogService blogService;
     @Resource
     private BlogEs blogEs;
-    @Test
-    public void testCreateIndex() throws IOException {
-        CreateIndexRequest request =  new CreateIndexRequest("blog_index");
-        CreateIndexResponse createIndexResponse =  client.indices().create(request, RequestOptions.DEFAULT);
-        System.out.println(createIndexResponse);
-    }
-    @Test
-    public void testUpdateIndex() throws Exception {
-        Blog blogById = blogService.findBlogById(110);
-        String id=String.valueOf(blogById.getId());
-//        ObjectMapper mapper = new ObjectMapper();
-//        String userJson = mapper.writeValueAsString(blogById);
-        blogEs.editBlog(id, JSON.toJSONString(blogById));
-    }
-    @Test
-    public void testDelIndex() throws IOException {
-        DeleteIndexRequest request = new DeleteIndexRequest("blog_index");
-        AcknowledgedResponse response = client.indices().delete(request, RequestOptions.DEFAULT);
-        // 响应状态
-        System.out.println(response.isAcknowledged());
-        client.close();
-    }
-    @Test
-    public void testDocInsert() throws Exception {
-        /* 插入数据 */
-        IndexRequest request = new IndexRequest();
-        //分页查询
-        int current=1;
-        int size =1000;
-        Blog blog = new Blog();
-        Page<Blog> page = new Page<Blog>(current,size);
-        IPage<Blog> blogIPage = blogService.findBlogByPage(page,blog);
-        //获取博客数据列表
-        List<Blog> blogList = blogIPage.getRecords();
-        blogEs.bluckBlog(blogList);
-//        for (Blog item:blogList) {
-//            System.out.println(item);
-//            Integer id = item.getId();
-//            request.index("blog_index").id(String.valueOf(id));
-//            // 向ES插入数据，必须将数据转换位JSON格式
-//            ObjectMapper mapper = new ObjectMapper();
-//            String userJson = mapper.writeValueAsString(item);
-//            System.out.println(userJson);
-//            request.source(userJson, XContentType.JSON);
-//            IndexResponse response = client.index(request, RequestOptions.DEFAULT);
-//            System.out.println(response.getResult());
-//        }
-        //查看详情
-//        Blog blog = blogService.findBlogById(39);
-//        blog.setReleaseDateStr(dates.format(blog.getReleaseDate(),'yyyy-MM-dd HH:mm:ss'));
-
+//    @Test
+//    public void testCreateIndex() throws IOException {
+//        CreateIndexRequest request =  new CreateIndexRequest("blog_index");
+//        CreateIndexResponse createIndexResponse =  client.indices().create(request, RequestOptions.DEFAULT);
+//        System.out.println(createIndexResponse);
+//    }
+//    @Test
+//    public void testUpdateIndex() throws Exception {
+//        Blog blogById = blogService.findBlogById(110);
+//        String id=String.valueOf(blogById.getId());
+//        blogEs.editBlog(id, JSON.toJSONString(blogById));
+//    }
+//    @Test
+//    public void testDelIndex() throws IOException {
+//        DeleteIndexRequest request = new DeleteIndexRequest("blog_index");
+//        AcknowledgedResponse response = client.indices().delete(request, RequestOptions.DEFAULT);
+//        // 响应状态
+//        System.out.println(response.isAcknowledged());
 //        client.close();
-    }
-    @Test
-    public void testGetDoc() throws IOException {
-        // 查询数据
-        GetRequest request = new GetRequest();
-        request.index("blog_index").id("39");
-        GetResponse response = client.get(request, RequestOptions.DEFAULT);
-        System.out.println(response.getSourceAsString());
-        client.close();
-    }
-    @Test
-    public void testSearchLight() throws IOException {
-        // 查询数据
-        String keyword = "java";
-        int pageNo = 4;
-        int pageSize = 2;
-        List<Map<String, Object>> list = blogEs.searchPage(keyword, pageNo, pageSize);
-        System.out.println(list);
-    }
-    @Test
-    public void testSearchDoc() throws IOException {
-        // 1. 查询索引的所有数据
-        SearchRequest request = new SearchRequest();
-        request.indices("blog_index");
-
-        // 构造查询条件
-        SearchSourceBuilder builder = new SearchSourceBuilder();
-        builder.query(QueryBuilders.matchQuery("title","java"));
-        //        SearchSourceBuilder builder = new SearchSourceBuilder();
-//        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+//    }
+//    @Test
+//    public void testDocInsert() throws Exception {
+//        /* 插入数据 */
+//        IndexRequest request = new IndexRequest();
+//        //分页查询
+//        int current=1;
+//        int size =1000;
+//        Blog blog = new Blog();
+//        Page<Blog> page = new Page<Blog>(current,size);
+//        IPage<Blog> blogIPage = blogService.findBlogByPage(page,blog);
+//        //获取博客数据列表
+//        List<Blog> blogList = blogIPage.getRecords();
+//        blogEs.bluckBlog(blogList);
+////        for (Blog item:blogList) {
+////            System.out.println(item);
+////            Integer id = item.getId();
+////            request.index("blog_index").id(String.valueOf(id));
+////            // 向ES插入数据，必须将数据转换位JSON格式
+////            ObjectMapper mapper = new ObjectMapper();
+////            String userJson = mapper.writeValueAsString(item);
+////            System.out.println(userJson);
+////            request.source(userJson, XContentType.JSON);
+////            IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+////            System.out.println(response.getResult());
+////        }
+//        //查看详情
+////        Blog blog = blogService.findBlogById(39);
+////        blog.setReleaseDateStr(dates.format(blog.getReleaseDate(),'yyyy-MM-dd HH:mm:ss'));
 //
-//        //boolQueryBuilder.must(QueryBuilders.matchQuery("age", 30));
-//        //boolQueryBuilder.must(QueryBuilders.matchQuery("sex", "男"));
-//        //boolQueryBuilder.mustNot(QueryBuilders.matchQuery("sex", "男"));
-//        boolQueryBuilder.should(QueryBuilders.matchQuery("age", 30));
-//        boolQueryBuilder.should(QueryBuilders.matchQuery("age", 40));
-        request.source(builder);
-
-        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
-
-        SearchHits hits = response.getHits();
-
-        System.out.println(response.getTook());
-        System.out.println(hits.getTotalHits());
-        Iterator<SearchHit> iterator = hits.iterator();
-        while (iterator.hasNext()) {
-            SearchHit hit = iterator.next();
-            System.out.println(hit.getSourceAsString());
-        }
-
-        client.close();
-
-    }
+////        client.close();
+//    }
+//    @Test
+//    public void testGetDoc() throws IOException {
+//        // 查询数据
+//        GetRequest request = new GetRequest();
+//        request.index("blog_index").id("39");
+//        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+//        System.out.println(response.getSourceAsString());
+//        client.close();
+//    }
+//    @Test
+//    public void testSearchLight() throws IOException {
+//        // 查询数据
+//        String keyword = "java";
+//        int pageNo = 4;
+//        int pageSize = 2;
+//        List<Map<String, Object>> list = blogEs.searchPage(keyword, pageNo, pageSize);
+//        System.out.println(list);
+//    }
+//    @Test
+//    public void testSearchDoc() throws IOException {
+//        // 1. 查询索引的所有数据
+//        SearchRequest request = new SearchRequest();
+//        request.indices("blog_index");
+//
+//        // 构造查询条件
+//        SearchSourceBuilder builder = new SearchSourceBuilder();
+//        builder.query(QueryBuilders.matchQuery("title","java"));
+//        //        SearchSourceBuilder builder = new SearchSourceBuilder();
+////        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+////
+////        //boolQueryBuilder.must(QueryBuilders.matchQuery("age", 30));
+////        //boolQueryBuilder.must(QueryBuilders.matchQuery("sex", "男"));
+////        //boolQueryBuilder.mustNot(QueryBuilders.matchQuery("sex", "男"));
+////        boolQueryBuilder.should(QueryBuilders.matchQuery("age", 30));
+////        boolQueryBuilder.should(QueryBuilders.matchQuery("age", 40));
+//        request.source(builder);
+//
+//        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+//
+//        SearchHits hits = response.getHits();
+//
+//        System.out.println(response.getTook());
+//        System.out.println(hits.getTotalHits());
+//        Iterator<SearchHit> iterator = hits.iterator();
+//        while (iterator.hasNext()) {
+//            SearchHit hit = iterator.next();
+//            System.out.println(hit.getSourceAsString());
+//        }
+//
+//        client.close();
+//
+//    }
 }
--- src/test/java/com/blog/MailServiceTest.java
+++ src/test/java/com/blog/MailServiceTest.java
@@ -14,59 +14,59 @@ public class MailServiceTest {
     @Autowired
     private MailUtil mailService;
     private TemplateEngine templateEngine;
-    @Test
-    public void sendSimpleMail() {
-        mailService.sendSimpleMail("Dev_guo@patazon.net","测试spring boot imail-主题","测试spring boot imail - 内容");
-    }
+//    @Test
+//    public void sendSimpleMail() {
+//        mailService.sendSimpleMail("Dev_guo@patazon.net","测试spring boot imail-主题","测试spring boot imail - 内容");
+//    }
+//
+//    @Test
+//    public void sendHtmlMail() throws  MessagingException {
+//
+//        String content = "<html>\n" +
+//                "<body>\n" +
+//                "<h3>hello world</h3>\n" +
+//                "<h1>html</h1>\n" +
+//                "<body>\n" +
+//                "</html>\n";
+//
+//        mailService.sendHtmlMail("Dev_guo@patazon.net","这是一封HTML邮件",content);
+//    }
 
-    @Test
-    public void sendHtmlMail() throws  MessagingException {
+//    @Test
+//    public void sendAttachmentsMail() throws MessagingException {
+//        String filePath = "新建 DOC 文档.doc";
+//        String content = "<html>\n" +
+//                "<body>\n" +
+//                "<h3>hello world</h3>\n" +
+//                "<h1>html</h1>\n" +
+//                "<h1>附件传输</h1>\n" +
+//                "<body>\n" +
+//                "</html>\n";
+//        mailService.sendAttachmentsMail("Dev_guo@patazon.net","这是一封HTML邮件",content, filePath);
+//    }
 
-        String content = "<html>\n" +
-                "<body>\n" +
-                "<h3>hello world</h3>\n" +
-                "<h1>html</h1>\n" +
-                "<body>\n" +
-                "</html>\n";
+//    @Test
+//    public void sendInlinkResourceMail() throws MessagingException {
+//        //TODO 改为本地图片目录
+//        String imgPath = "4.png";
+//        String rscId = "admxj001";
+//        String content = "<html>" +
+//                "<body>" +
+//                "<h3>hello world</h3>" +
+//                "<h1>html</h1>" +
+//                "<h1>图片邮件</h1>" +
+//                "<img src='cid:"+rscId+"'></img>" +
+//                "<body>" +
+//                "</html>";
+//
+//        mailService.sendInlinkResourceMail("Dev_guo@patazon.net","这是一封图片邮件",content, imgPath, rscId);
+//    }
 
-        mailService.sendHtmlMail("Dev_guo@patazon.net","这是一封HTML邮件",content);
-    }
-
-    @Test
-    public void sendAttachmentsMail() throws MessagingException {
-        String filePath = "新建 DOC 文档.doc";
-        String content = "<html>\n" +
-                "<body>\n" +
-                "<h3>hello world</h3>\n" +
-                "<h1>html</h1>\n" +
-                "<h1>附件传输</h1>\n" +
-                "<body>\n" +
-                "</html>\n";
-        mailService.sendAttachmentsMail("Dev_guo@patazon.net","这是一封HTML邮件",content, filePath);
-    }
-
-    @Test
-    public void sendInlinkResourceMail() throws MessagingException {
-        //TODO 改为本地图片目录
-        String imgPath = "/ijiangtao/img/blob/dd9899b4cf95cbf074ddc4607007046c022564cb/blog/animal/dog/dog-at-work-with-computer-2.jpg?raw=true";
-        String rscId = "admxj001";
-        String content = "<html>" +
-                "<body>" +
-                "<h3>hello world</h3>" +
-                "<h1>html</h1>" +
-                "<h1>图片邮件</h1>" +
-                "<img src='cid:"+rscId+"'></img>" +
-                "<body>" +
-                "</html>";
-
-        mailService.sendInlinkResourceMail("Dev_guo@patazon.net","这是一封图片邮件",content, imgPath, rscId);
-    }
-
-    @Test
-    public void testTemplateMailTest() throws MessagingException {
-        Context context = new Context();
-        context.setVariable("id","ispringboot");
-        String emailContent = templateEngine.process("emailTeplate", context);
-        mailService.sendHtmlMail("Dev_guo@patazon.net","这是一封HTML模板邮件",emailContent);
-    }
+//    @Test
+//    public void testTemplateMailTest() throws MessagingException {
+//        Context context = new Context();
+//        context.setVariable("id","ispringboot");
+//        String emailContent = templateEngine.process("emailTeplate", context);
+//        mailService.sendHtmlMail("Dev_guo@patazon.net","这是一封HTML模板邮件",emailContent);
+//    }
 }
